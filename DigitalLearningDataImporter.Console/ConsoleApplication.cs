d1eda8a63f92a8404ed1e3b04c1deb86708d2c27
mobildeva <mobildeva@gmail.com>
2020-09-10 17:40:31 -0400

    Changed version

--- DigitalLearningDataImporter.Console/ConsoleApplication.cs
+++ DigitalLearningDataImporter.Console/ConsoleApplication.cs
@@ -71,7 +71,7 @@ namespace DigitalLearningDataImporter.Console
 
                 Log.Information("------------------------------------------------");
 
-                Log.Information("Starting the app. Version: 1.7");
+                Log.Information("Starting the app. Version: 1.8");
 
                 var txtFilePath = @Environment.CurrentDirectory + "\\" + DateTime.Now.ToString("yyyyMMdd") + "_Monitoreo_ImportExcelToDL.txt";
 
